9ebc240a93f31134da37cc9f0b2da1d3b3983492
Ritik Raj <ritikrajbarh18@gmail.com>
2022-10-11 01:07:13 +0530

    Fibal commit for Calculator project for 10th Oct

--- calculator.js
+++ calculator.js
@@ -2,3 +2,15 @@ function display(value)
 {
 	document.getElementById('textArea').value += value
 }
+
+function calculate()
+{
+	var value = document.getElementById('textArea').value
+	var answer = eval(value)
+	document.getElementById('textArea').value = answer
+}
+
+function clr()
+{
+	document.getElementById('textArea').value =''
+}
